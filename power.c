764326a45ec0cd03b0e3400e05c20a972004a189
GORAKH GUPTA <gorakhgupta999@gmail.com>
2022-10-01 01:31:35 +0530

    added recursive method to find the power of a number.

--- power.c
+++ power.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+
+
+int power_recursive(int base, int a) {
+    if (a != 0)
+        return (base * power(base, a - 1));
+    else
+        return 1;
+}
+
+// Find power of a number using Iterative Mode.
+long long int power_iterative(int m,int n){
+    for (int i = 0; i < n; i++)
+    {
+        val = val * m;
+    }
+    
+    return val;
+}
 int main()
 {
     int m, n;
@@ -7,11 +25,10 @@ int main()
     scanf("%d", &m);
     printf("Enter the power of that number\n");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-        val = val * m;
-    }
-    printf("The value of %d to the power %d is: %d", m, n, val);
+    
+    printf("The value of %d to the power %d using Iterative Method is: %d", m, n, power_iterative(m, n));
+    
+    printf("The value of %d to the power %d using Recursive Method is: %d", m, n, power_recursive(m, n));
     
     return 0;
 }
